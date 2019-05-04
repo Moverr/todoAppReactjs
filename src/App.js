56434f78e22983b2ef7ebdad442d5d581f6f1717
Muyinda Rogers <moverr@gmail.com>
2019-05-04 19:23:04 +0300

    :Debugigng  App :

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './css/App.css';
 import PropTypes from 'prop-types';
-import InputField from './shared/forms/fields/Utilities';
+import InputField from './shared/forms/fields/InputField';
 
 const propTypes = {};
 
